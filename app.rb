6eac87fa0b60d47d146bdd70ccd4aad45a285163
Chris Mytton <chrismytton@gmail.com>
2017-08-15 16:17:44 +0100

    Read wiki username and password from secrets.yml

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'bundler/setup'
 require 'compare_with_wikidata'
 require 'sinatra'
+require 'yaml'
+
+begin
+  secrets = YAML.load_file('secrets.yml')
+  ENV['WIKI_USERNAME'] = secrets['WIKI_USERNAME']
+  ENV['WIKI_PASSWORD'] = secrets['WIKI_PASSWORD']
+rescue Errno::ENOENT
+  abort "Please run 'cp secrets.yml-example secrets.yml' and then fill out secrets.yml"
+end
 
 get '/' do
   redirect to('/prompter')
